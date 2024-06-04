9a35f6b8e8586335c546010854cb4f16916f669a
chanansiri <nuanpang_c@silpakorn.edu>
2024-06-04 10:12:44 +0700

    5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServiceService } from './service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Line_liff';
+  
+  constructor(private service: ServiceService) { }
+  ngOnInit(): void {
+    this.service.lineliff();
+  }
+
 }
